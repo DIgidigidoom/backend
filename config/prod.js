c8473d24b96c92d79072f96c03254d02d2db15bd
Digidigidoom <tomshahar91@gmail.com>
2025-05-18 16:43:24 +0300

    Fix

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://TomShahar:Ts0268420@cluster0.v43k6ve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://TomShahar:Ts0268420@cluster0.v43k6ve.mongodb.net/StupifyDB?retryWrites=true&w=majority&appName=Cluster0',
   dbName : process.env.DB_NAME || 'tester_db'
 }
