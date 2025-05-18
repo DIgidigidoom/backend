35f66acdf26063332bd7ccd15b6df344540f72d0
Digidigidoom <tomshahar91@gmail.com>
2025-05-18 16:40:41 +0300

    Change production dbURL

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://TomShahar:Ts0268420@cluster0.v43k6ve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   dbName : process.env.DB_NAME || 'tester_db'
 }
