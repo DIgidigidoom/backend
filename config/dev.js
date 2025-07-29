baf319241fa4c4bcac1f360ec00a04a3740a6902
bnethanel <bnethanel@gmail.com>
2025-07-29 18:59:26 +0300

    fix security check

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,8 @@
-export default  {
-  dbURL: 'mongodb://127.0.0.1:27017',
-  // dbURL: 'mongodb+srv://NateBuz:NateBuz123@cluster0.v43k6ve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  dbName : 'StupifyDB'
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  dbURL: process.env.MONGO_URI,
+  dbName: process.env.DB_NAME
 }
 
